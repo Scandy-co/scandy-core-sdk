7602c263f2cd0e33c28a0ed31b9e8e103b9eb493
H. Cole Wiley <cole@scandy.co>
2017-04-07 12:40:04 -0500

    bumped build version

--- include/scandy_version.h
+++ include/scandy_version.h
@@ -13,13 +13,13 @@
 #define scandy_version_h
 
 #define ScandyCore_VERSION_STRING "v0.1.1"
-#define ScandyCore_VERSION_STRING_FULL "v0.1.1+62.3e2dfb5a"
+#define ScandyCore_VERSION_STRING_FULL "v0.1.1+68.5d6ed197"
 #define ScandyCore_VERSION "0.1.1"
 #define ScandyCore_VERSION_MAJOR 0
 #define ScandyCore_VERSION_MINOR 1
 #define ScandyCore_VERSION_PATCH 1
 #define ScandyCore_VERSION_TWEAK ""
-#define ScandyCore_VERSION_AHEAD 62
-#define ScandyCore_VERSION_GIT_SHA "3e2dfb5a"
+#define ScandyCore_VERSION_AHEAD 68
+#define ScandyCore_VERSION_GIT_SHA "5d6ed197"
 
 #endif // scandy_version_h
