312b89193874b749aad730abaf34be93894e9757
elaughli <el.laughlin.el@gmail.com>
2017-06-12 14:36:10 -0500

    temporarily put gen_uid helper function in ID_Types during refactor

--- include/scandy/utilities/ID_types.h
+++ include/scandy/utilities/ID_types.h
@@ -18,6 +18,7 @@
 #define Scandy_ID_types_h
 
 #include <string>
+#include <cstdlib>
 
 namespace scandy { namespace utilities {
 
@@ -27,6 +28,28 @@ using StreamID = std::string;
 using MetadataUID = std::string;
 using SensorID = std::string;
 using DeviceID = std::string;
+
+// testing function for generating a uid
+// TODO DON'T actually use this!. use the database one and find better solution
+// for memory track store
+inline void gen_uid(MetadataUID& uid, const int len) {
+  char *s = (char *)malloc(sizeof(char)*(len+1));
+
+  static const char alphanum[] =
+    "0123456789"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz";
+
+  for (int i = 0; i < len; ++i) {
+    s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+  }
+
+    s[len] = 0;
+
+    uid = s;
+
+    free(s);
+}
 
 }}
 
